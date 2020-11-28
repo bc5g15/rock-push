018a13910fe8aca7944f05c6487aeb55faebf578
Benedict Cairns <bcairns21@gmail.com>
2020-11-28 07:38:17 +0000

    Initial menu mockup

---
+++ Prefabs/scripts/OverlayMenu.gd
@@ -0,0 +1,34 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var menuBox = $MenuBox
+onready var menuButton = $MenuButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_MenuButton_toggled(button_pressed):
+	menuBox.visible = button_pressed
+	pass # Replace with function body.
+
+
+func _on_CloseMenuButton_pressed():
+	menuButton.pressed = false
+	menuBox.visible = false
+	pass # Replace with function body.
+
+
+func _on_MainMenuButton_pressed():
+	Global.goto_scene('res://Debug.tscn')
+	pass # Replace with function body.
