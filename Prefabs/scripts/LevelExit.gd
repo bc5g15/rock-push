d4f573195ffc42b071f9aec203b6f819163ee1bd
Benedict Cairns <bcairns21@gmail.com>
2020-11-30 09:21:50 +0000

    Level exit uses global fade controller

--- Prefabs/scripts/LevelExit.gd
+++ Prefabs/scripts/LevelExit.gd
@@ -2,16 +2,5 @@ extends Area2D
 
 export(String) var path = ""
 
-onready var fadeAnimator = $FadeAnimator
-
-func _ready():
-	fadeAnimator.play("FadeIn")
-
 func _on_LevelExit_body_entered(_body):
-	fadeAnimator.play("FadeOut")
-
-func _on_FadeAnimator_animation_finished(anim_name):
-	if anim_name == "FadeOut":
-		if path != "":
-			print("Enter Level: " + path)
-			Global.goto_scene("res://" + path + ".tscn")
+	FadeController.fade_to_scene("res://" + path + ".tscn")
