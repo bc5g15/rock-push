27c754cdb10148ea41b0972f599dac36e17234e1
Benedict Cairns <bcairns21@gmail.com>
2020-11-30 09:22:06 +0000

    PlayerFollow now uses onready var

--- TopScripts/PlayerFollow.gd
+++ TopScripts/PlayerFollow.gd
@@ -5,10 +5,11 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var viewport: Viewport
+onready var camera = $Player/Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Player/Camera2D.make_current()
+	camera.make_current()
 	pass # Replace with function body.
 
 
