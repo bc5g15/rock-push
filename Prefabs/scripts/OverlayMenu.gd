08684bc35ad0ea3b83015095d040a1d9e783cf6e
Benedict Cairns <bcairns21@gmail.com>
2020-11-30 09:08:02 +0000

    Use FadeController in the overlay menu

--- Prefabs/scripts/OverlayMenu.gd
+++ Prefabs/scripts/OverlayMenu.gd
@@ -8,36 +8,19 @@ extends Control
 onready var menuBox = $MenuBox
 onready var menuButton = $HBoxContainer/MenuButton
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_MenuButton_toggled(button_pressed):
 	menuBox.visible = button_pressed
 	Global.set_pause(button_pressed)
-	pass # Replace with function body.
-
 
 func _on_CloseButton_pressed():
 	menuButton.pressed = false
 	menuBox.visible = false	
 	Global.set_pause(false)
-	pass # Replace with function body.
-
 
 func _on_ResetButton_pressed():
 	Global.set_pause(false)
-	Global.reset_current_scene()
-	pass # Replace with function body.
-
+	FadeController.fade_reset()
 
 func _on_HomeButton_pressed():
 	Global.set_pause(false)
-	Global.goto_scene('res://Debug.tscn')	
-	pass # Replace with function body.
+	FadeController.fade_to_scene('res://Debug.tscn')
