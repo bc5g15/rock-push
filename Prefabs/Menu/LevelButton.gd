65b94fd29d5399377c3df2e9a0b1ba56ade2e936
Benedict Cairns <bcairns21@gmail.com>
2020-12-06 08:55:53 +0000

    Add level selection buttons

---
+++ Prefabs/Menu/LevelButton.gd
@@ -0,0 +1,26 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export(String) var level = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_check_locked()
+
+func _check_locked():
+	if text:
+		disabled = LevelUnlocker.isLocked(text)
+		print(disabled)
+
+func _pressed():
+	if text:
+		FadeController.fade_to_scene("res://Levels/" + text + ".tscn")
+
+
+func refresh():
+	print("Hello?")
+	_check_locked()
+	pass # Replace with function body.
---
+++ Prefabs/Menu/UnlockAllButton.gd
@@ -0,0 +1,14 @@
+extends Button
+
+#onready var buttons = [$]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _pressed():
+	LevelUnlocker.unlock_all()
+	get_tree().call_group("LevelButtons", "refresh")
