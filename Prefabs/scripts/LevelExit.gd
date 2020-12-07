3b161023c63370f1666ffa4d0bf6f96f2c109bc9
Benedict Cairns <bcairns21@gmail.com>
2020-12-07 07:52:50 +0000

    Unlock next level on exit

--- Prefabs/scripts/LevelExit.gd
+++ Prefabs/scripts/LevelExit.gd
@@ -3,4 +3,7 @@ extends Area2D
 export(String) var path = ""
 
 func _on_LevelExit_body_entered(_body):
+	if "Levels/" in path:
+		var level = path.right(7)
+		LevelUnlocker.unlock(level)
 	FadeController.fade_to_scene("res://" + path + ".tscn")
