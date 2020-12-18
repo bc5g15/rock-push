2433601f9c5f2c0afb6bef4ef40f62ffd9db1946
Benedict Cairns <bcairns21@gmail.com>
2020-12-18 07:04:23 +0000

    Success blip on level exit

--- Prefabs/scripts/LevelExit.gd
+++ Prefabs/scripts/LevelExit.gd
@@ -3,8 +3,10 @@ extends Area2D
 export(String) var path = ""
 export(String) var current = ""
 
+onready var victorySFX = $VictoryBlip
 
 func _on_LevelExit_body_entered(_body):
+	victorySFX.play()
 	if "Levels/" in current:
 		var l1 = current.right(7)
 		LevelUnlocker.complete(l1)
