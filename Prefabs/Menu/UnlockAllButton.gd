08524d24d7c107d79961afd02bfbfccda0ab7040
Benedict Cairns <bcairns21@gmail.com>
2020-12-21 07:06:37 +0000

    Add icon to unlock button

--- Prefabs/Menu/UnlockAllButton.gd
+++ Prefabs/Menu/UnlockAllButton.gd
@@ -10,6 +10,8 @@ func _ready():
 #func _process(delta):
 #	pass
 func _pressed():
+	text = "Unlocked!"
+	icon = load("res://Assets/UI/baseline_lock_open_white_48dp.png")
 	FadeController.blip()
 	LevelUnlocker.unlock_all()
 	get_tree().call_group("LevelButtons", "refresh")
